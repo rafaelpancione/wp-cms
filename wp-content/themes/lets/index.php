26fa3bf8b9ef229273e642b0ed0fc6cf1d455ef9
rafaelpancione <rafaelpancione@hotmail.com>
2018-10-30 16:14:54 -0300

    Configuração inicial para WP

---
+++ wp-content/themes/lets/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/grid.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Let´s Go Pizzas</title>
+</head> 
+<body>
+
+	<header class="header">
+		<div class="container">
+			<nav class="grid-16 menu">
+				<ul>
+					<li><a href="index.html">Início</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+
+
+	<section class="main">
+		<div class="container">
+		<h1>Pizzas</h1>
+		<h4>São mais de 20 sabores.</h4>
+		<div class="pizza-salgada grid-8">
+			<h2>Salgadas</h2>
+		<ul class="lista-pizza">
+		<li><h3>3 Queijos</h3><p>Mussarela, requeijão e parmesão ralado.</p></li>
+		<li><h3>Frango com requeijão</h3><p>Frango desfiado, cebola e requeijão.</p></li>		
+		<li><h3>Bauru</h3><p>Mussarela, presunto, requeijão e tomate.</p></li>
+		<li><h3>Calabresa</h3><p>
+Mussarela, calabresa e cebola.</p></li>
+		<li><h3>Mussarela</h3><p>Queijo mussarela.</p></li>
+		<li><h3>Margherita</h3><p>Mussarela, tomate e manjericão.</p></li>
+		<li><h3>Pepperoni</h3><p>Mussarela e pepperoni.</p></li>
+		<li><h3>Presunto</h3><p>Mussarela e presunto.</p></li>
+		<li><h3>Napolitana</h3><p>Mussarela, tomate e parmesão ralado.</p></li>
+		<li><h3>América</h3><p>Mussarela, pepperoni, champignon, cebola e pimentão verde.</p></li>
+		<li><h3>4 Queijoss</h3><p>Mussarela, requeijão, gorgonzola e parmesão ralado.</p></li>
+		<li><h3>La Bianca</h3><p>Mussarela, mussarela de búfala, requeijão, parmesão ralado e manjericão.</p></li>
+		<li><h3>Calabresa Especial</h3><p>Mussarela, azeitona preta, calabresa, cebola e Philadelphia®.</p></li>
+		<li><h3>Catuperoni</h3><p>Mussarela, pepperoni, requeijão e parmesão ralado.</p></li>
+		<li><h3>
+Portuguesa</h3><p>Mussarela, presunto, ovo de codorna, azeitona preta, cebola e pimentão verde.</p></li>
+		</ul>	
+		</div>
+		
+		<div class="pizza-doce grid-8">
+		<h2>Doces</h2>
+		<ul class="lista-pizza">
+			
+		<li><h3>Banana</h3><p>
+		Leite condensado, banana, açúcar e canela.</p></li>
+
+		<li><h3>Banana Crocante</h3><p>
+		Mussarela, leite condensado, banana e farofa doce.</p></li>
+
+		<li><h3>Beijinho</h3><p>
+		Leite condensado, mussarela, chocolate branco e côco.</p></li>
+
+		<li><h3>Brigadeiro</h3><p>
+		Leite condensado, mussarela, chocolate, chocolate granulado e cereja.</p></li>
+
+		<li><h3>Chocobana</h3><p>
+		Leite condensado, mussarela, banana e chocolate.</p></li>
+	
+		<li><h3>Chocolate</h3><p>
+		Leite condensado, mussarela e chocolate ao leite.</p></li>
+
+		<li><h3>Chocolate Branco</h3><p>
+		Leite condensado, mussarela e chocolate branco.</p></li>
+
+		<li><h3>Chokito</h3><p>
+		Leite condensado, mussarela, chocolate, flocos de arroz e caramelo.</p></li>
+
+		<li><h3>Cocada</h3><p>
+		Mussarela, leite condensado, cocada cremosa.</p></li>
+
+		<li><h3>Confeti</h3><p>
+		Leite condensado, mussarela, chocolate ao leite e confeti.</p></li>
+
+		<li><h3>Prestígio</h3><p>
+		Leite condensado, mussarela, chocolate e côco.</p></li>
+
+		<li><h3>Sensação</h3><p>
+		Leite condensado, mussarela, morango, chocolate ao leite.</p></li>
+
+		</ul>	
+		</div>
+		</div>
+	</section>
+
+
+	<footer class="footer container">
+		<div class="container">
+		<p>Let´s Go Pizzas, 2018.</p>
+		</div>
+	</footer>
+
+</body>
+</html>
